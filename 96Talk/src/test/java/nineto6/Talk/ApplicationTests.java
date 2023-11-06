2cdd3f3f6bac918b3effe0bdfde7ffa32360dd22
chang-seop <ikavon@naver.com>
2023-11-06 11:57:33 +0900

    SET: SpringBoot and default setting

---
+++ 96Talk/src/test/java/nineto6/Talk/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nineto6.Talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
