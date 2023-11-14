b4e23d77a9b5fe1ca9df514869e2bd9ff237a3ec
chang-seop <ikavon@naver.com>
2023-11-14 15:16:43 +0900

    FEAT: Register SSL certificate for local use
    * feat Application : ssl-local.properties 등록
    * add ssl-local.properties : SSL key 등록 및 설정

--- 96Talk/src/main/java/nineto6/Talk/Application.java
+++ 96Talk/src/main/java/nineto6/Talk/Application.java
@@ -2,8 +2,10 @@ package nineto6.Talk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:ssl-local.properties")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
