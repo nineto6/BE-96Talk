ea7ef3036eec314769644e9c81dec07a1cc6739e
chang-seop <ikavon@naver.com>
2023-11-18 03:51:04 +0900

    FIX: Remove SSL
    * modify Application
    * remove ssl-local.properties

--- 96Talk/src/main/java/nineto6/Talk/Application.java
+++ 96Talk/src/main/java/nineto6/Talk/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:ssl-local.properties")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
