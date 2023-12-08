7490cf2a5fc6ddbbadde3718655115ab779ae3e5
chang-seop <ikavon@naver.com>
2023-12-09 01:06:44 +0900

    FIX: FriendService에서 친구 생성 로직 변경
    자기 자신을 친구 추가 할 경우 친구가 되는 문제 발생

--- 96Talk/src/main/java/nineto6/Talk/domain/friend/service/FriendService.java
+++ 96Talk/src/main/java/nineto6/Talk/domain/friend/service/FriendService.java
@@ -23,16 +23,21 @@ public class FriendService {
     private final MemberRepository memberRepository;
     @Transactional
     public void createFriend(MemberDto memberDto, FriendRequest friendRequest) {
+        // 자기 자신을 친구 추가 할 경우 Exception
+        if(memberDto.getMemberNickname().equals(friendRequest.getFriendNickname())) {
+            throw new BusinessExceptionHandler(ErrorCode.BAD_REQUEST_ERROR);
+        }
+
         // 이름으로 된 친구가 등록되어 있는지 확인
         Member friendMember = memberRepository.findByMemberNickname(friendRequest.getFriendNickname())
-                .orElseThrow(() -> new BusinessExceptionHandler(ErrorCode.BUSINESS_EXCEPTION_ERROR));
+                .orElseThrow(() -> new BusinessExceptionHandler(ErrorCode.BAD_REQUEST_ERROR));
 
         // 이미 등록되어 있는 친구인지 확인
         Optional<Friend> friendOptional = friendRepository.findByMemberIdAndFriendMemberId(memberDto.getMemberId(), friendMember.getMemberId());
 
         if(friendOptional.isPresent()) {
             // 이미 등록되어 있는 경우 Exception
-            throw new BusinessExceptionHandler(ErrorCode.BUSINESS_EXCEPTION_ERROR);
+            throw new BusinessExceptionHandler(ErrorCode.BAD_REQUEST_ERROR);
         }
 
         Friend friend = Friend.builder()
