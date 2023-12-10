f83d9f9d1c2b1d65dab008200513011b8171f65c
chang-seop <ikavon@naver.com>
2023-12-10 14:12:21 +0900

    CHORE: MongoDB 추가 및 설정

--- 96Talk/src/main/java/nineto6/Talk/Application.java
+++ 96Talk/src/main/java/nineto6/Talk/Application.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @OpenAPIDefinition(servers = {
 		@Server(url = "http://localhost:8080/api", description = "Local Server URL"),
 		@Server(url = "http://nineto6.kro.kr:8080/api", description = "Develop Server URL"),
 		@Server(url = "https://nineto6.p-e.kr/api", description = "Product Server URL")})
 @SpringBootApplication
+@EnableMongoRepositories
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
