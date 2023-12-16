caf1c7c5b1d76e48ab42dc66f6ce5f88e64ac045
chang-seop <ikavon@naver.com>
2023-12-16 23:21:18 +0900

    TEST: ChatroomMember 테스트 추가 및 테스트 DB 설정 변경

--- 96Talk/src/test/java/nineto6/Talk/domain/chatroom/chatroommember/repository/ChatroomMemberRepositoryTest.java
+++ 96Talk/src/test/java/nineto6/Talk/domain/chatroom/chatroommember/repository/ChatroomMemberRepositoryTest.java
@@ -2,17 +2,19 @@ package nineto6.Talk.domain.chatroom.chatroommember.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import nineto6.Talk.domain.chatroom.chatroommember.domain.ChatroomMember;
+import nineto6.Talk.domain.chatroom.chatroommember.domain.ChatroomMemberAndChannelId;
+import nineto6.Talk.domain.chatroom.chatroommember.domain.ChatroomMemberAndNickname;
 import nineto6.Talk.domain.chatroom.domain.Chatroom;
 import nineto6.Talk.domain.chatroom.repository.ChatroomRepository;
 import nineto6.Talk.domain.member.domain.Member;
 import nineto6.Talk.domain.member.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -95,5 +97,93 @@ public class ChatroomMemberRepositoryTest {
         assertThat(chatroomOptional.orElse(null)).isNotNull();
         assertThat(chatroomOptional.get().getMemberId()).isEqualTo(member.getMemberId());
         assertThat(chatroomOptional.get().getChatroomId()).isEqualTo(chatroom.getChatroomId());
+    }
+
+    @Test
+    void updateSubDateByChannelIdAndMemberId() {
+        // given
+        chatroomMemberRepository.save(chatroomMember);
+
+        // when
+        chatroomMemberRepository.updateSubDateByChannelIdAndMemberId(chatroom.getChatroomChannelId(), member.getMemberId());
+
+        // then
+        Optional<ChatroomMember> chatroomOptional = chatroomMemberRepository.findByChatroomIdAndMemberId(chatroom.getChatroomId(), member.getMemberId());
+        assertThat(chatroomOptional.orElse(null)).isNotNull();
+        assertThat(chatroomOptional.get().getChatroomSubDate()).isNotNull();
+        assertThat(chatroomOptional.get().getChatroomUnSubDate()).isNull();
+    }
+
+    @Test
+    void removeSubDateByChannelIdAndMemberId() {
+        // given
+        chatroomMemberRepository.save(chatroomMember);
+        chatroomMemberRepository.updateSubDateByChannelIdAndMemberId(chatroom.getChatroomChannelId(), member.getMemberId());
+
+        // when
+        chatroomMemberRepository.removeSubDateByChannelIdAndMemberId(chatroom.getChatroomChannelId(), member.getMemberId());
+
+        // then
+        Optional<ChatroomMember> chatroomOptional = chatroomMemberRepository.findByChatroomIdAndMemberId(chatroom.getChatroomId(), member.getMemberId());
+        assertThat(chatroomOptional.orElse(null)).isNotNull();
+        assertThat(chatroomOptional.get().getChatroomSubDate()).isNull();
+        assertThat(chatroomOptional.get().getChatroomUnSubDate()).isNull();
+    }
+
+    @Test
+    void updateUnSubDateByChannelIdAndMemberId() {
+        // given
+        chatroomMemberRepository.save(chatroomMember);
+
+        // when
+        chatroomMemberRepository.updateUnSubDateByChannelIdAndMemberId(chatroom.getChatroomChannelId(), member.getMemberId());
+
+        // then
+        Optional<ChatroomMember> chatroomOptional = chatroomMemberRepository.findByChatroomIdAndMemberId(chatroom.getChatroomId(), member.getMemberId());
+        assertThat(chatroomOptional.orElse(null)).isNotNull();
+        assertThat(chatroomOptional.get().getChatroomSubDate()).isNull();
+        assertThat(chatroomOptional.get().getChatroomUnSubDate()).isNotNull();
+    }
+
+    @Test
+    void findByChannelIdAndNickname() {
+        // given
+        chatroomMemberRepository.save(chatroomMember);
+
+        // 다른 사용자 등록
+        Member friend = Member.builder()
+                .memberEmail("test1@naver.com")
+                .memberPwd("123123")
+                .memberNickname("한국1")
+                .build();
+        memberRepository.save(friend);
+
+        ChatroomMember chatroomFriend = ChatroomMember.builder()
+                .chatroomId(chatroom.getChatroomId())
+                .memberId(friend.getMemberId())
+                .build();
+        chatroomMemberRepository.save(chatroomFriend);
+
+        // when
+        List<ChatroomMemberAndNickname> chatroomMemberList = chatroomMemberRepository.findByChannelIdAndNickname(chatroom.getChatroomChannelId(), member.getMemberNickname());
+
+        // then
+        assertThat(chatroomMemberList.size()).isEqualTo(1);
+        assertThat(chatroomMemberList.get(0).getMemberId()).isEqualTo(friend.getMemberId());
+        assertThat(chatroomMemberList.get(0).getMemberNickname()).isEqualTo(friend.getMemberNickname());
+    }
+
+    @Test
+    void findByMemberId() {
+        // given
+        chatroomMemberRepository.save(chatroomMember);
+
+        // when
+        List<ChatroomMemberAndChannelId> chatroomMemberList = chatroomMemberRepository.findByMemberId(member.getMemberId());
+
+        // then
+        assertThat(chatroomMemberList.size()).isEqualTo(1);
+        assertThat(chatroomMemberList.get(0).getChatroomId()).isEqualTo(chatroom.getChatroomId());
+        assertThat(chatroomMemberList.get(0).getChannelId()).isEqualTo(chatroom.getChatroomChannelId());
     }
 }
